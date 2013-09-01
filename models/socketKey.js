dafabcd9d4f2f6c66cffd02ce53295123724bf49
Ilya Kantor <iliakan@gmail.com>
2013-09-01 18:39:49 +0400

    better auth

---
+++ models/socketKey.js
@@ -0,0 +1,24 @@
+var mongoose = require('db');
+var Schema = mongoose.Schema;
+
+var config = require('config');
+
+var schema = new Schema({
+  socketKey: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  sessionId: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: config.get('socketKey:maxAge')
+  }
+});
+
+exports.SocketKey = mongoose.model('SocketKey', schema);
