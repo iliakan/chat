a6c8ae69d6cba178e9ff70c805d10206577068a9
Ilya Kantor <iliakan@gmail.com>
2013-09-02 13:17:11 +0400

    auto reconnect

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,22 +1,31 @@
-function Connection() {
+function Connection(options) {
+  this.options = options || {};
 
-  this.connection = new SockJS('/socket');
-
-  this.status = this.HANDSHAKE;
-
-  this.connection.onopen = this.onOpen.bind(this);
-  this.connection.onmessage = this.onMessage.bind(this);
-  this.connection.onclose = this.onClose.bind(this);
+  this.reconnectAttempt = 0;
+  this.connect();
 }
 
 jQuery.extend(Connection.prototype, $.eventEmitter);
 
-['HANDSHAKE', 'OPEN', 'CLOSED'].forEach(function(status) {
+['CONNECTING', 'OPEN', 'CLOSED'].forEach(function(status) {
   Connection.prototype[status] = status;
 });
 
+Connection.prototype.RECONNECT_DELAYS = [1000, 2500, 5000, 10000, 30000, 60000];
+
+Connection.prototype.connect = function() {
+  this.socket = new SockJS(this.options.prefix, undefined, this.options.socket);
+  this.status = this.CONNECTING;
+
+  this.socket.onopen = this.onOpen.bind(this);
+  this.socket.onmessage = this.onMessage.bind(this);
+  this.socket.onclose = this.onClose.bind(this);
+}
+
 Connection.prototype.onOpen = function() {
   var self = this;
+
+  this.reconnectAttempt = 0;
 
   $.ajax({
     method: 'POST',
@@ -35,7 +44,7 @@ Connection.prototype.onMessage = function(e) {
 
   var message = JSON.parse(e.data);
 
-  if (this.status == this.HANDSHAKE) {
+  if (this.status == this.CONNECTING) {
     if (message.type == 'handshake') {
       this.status = this.OPEN;
       this.emit('open');
@@ -49,13 +58,36 @@ Connection.prototype.onMessage = function(e) {
 };
 
 Connection.prototype.onClose = function(event) {
-  var wasStatus = this.status;
-  this.status = this.CLOSED;
-  if (wasStatus == this.HANDSHAKE) {
-    this.emit('close', event);
+  var self = this;
+
+  if (this.options.debug) {
+    console.log(event);
   }
+
+  if (event.code == 401) {
+    // logged out
+    this.status = this.CLOSED;
+    this.emit('close', event);
+    return;
+  }
+
+  this.status = this.CONNECTING;
+  this.emit('disconnect');
+
+  var delay = this.RECONNECT_DELAYS[this.reconnectAttempt]
+  || this.RECONNECT_DELAYS[this.RECONNECT_DELAYS.length-1];
+
+  if (this.options.debug) {
+    console.log("Reconnect in " + delay);
+  }
+
+  setTimeout(function() {
+    self.reconnectAttempt++;
+    self.connect();
+  }, delay);
+
 };
 
 Connection.prototype.send = function(message) {
-  this.connection.send(JSON.stringify(message));
+  this.socket.send(JSON.stringify(message));
 };
